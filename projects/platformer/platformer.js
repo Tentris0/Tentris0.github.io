9161103951d86dc90020a7fb415f5bc7a8033579
Christina Neuman <Christina.Neuman@jpschools.org>
2024-06-10 16:31:50 +0000

    added platforms and collectables to game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -44,6 +44,9 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
+   createPlatform(500, 300, 200, 20); 
+   
+  
 
 
     
@@ -52,6 +55,7 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
+    createCollectable("halliebOt", 500, 300, 20, 0.5);
 
 
 
