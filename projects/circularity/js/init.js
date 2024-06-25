8b309e8dc702056b63ac1a7e8d0f5268619e02b2
Christina Neuman <Christina.Neuman@jpschools.org>
2024-06-25 23:03:46 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -33,7 +33,7 @@ var init = function (window) {
 
 
             // set motion properties
-            physikz.addRandomVelocity(circle, canvas);
+            physikz.addRandomVelocity(circle, canvas, 12, 12);
 
             // adds the circle to circles array, add to view
             circles.push(circle);
@@ -41,15 +41,16 @@ var init = function (window) {
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++){
-            console.log(loopsCompleted[0]);
-        }
+
         // * deleted functions * //
         //drawCircle();
         //drawCircle();
         //drawCircle();
         //drawCircle();
         //drawCircle();
+        for (var circ100 = 0; circ100 < 100; circ100+=1){
+            drawCircle();
+        }
 
 
         ////////////////////////////////////////////////////////////
@@ -64,25 +65,25 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);
-            physikz.updatePosition(circles[4]);
+            //physikz.updatePosition(circles[0]);
+            //physikz.updatePosition(circles[1]);
+            //physikz.updatePosition(circles[2]);
+            //physikz.updatePosition(circles[3]);
+            //physikz.updatePosition(circles[4]);
             //Deleted circle positions 
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           game.checkCirclePosition(circles[0])
-           game.checkCirclePosition(circles[1])
-           game.checkCirclePosition(circles[2])
-           game.checkCirclePosition(circles[3])
-           game.checkCirclePosition(circles[4])
+           //game.checkCirclePosition(circles[0]);
+           //game.checkCirclePosition(circles[1]);
+           //game.checkCirclePosition(circles[2]);
+           //game.checkCirclePosition(circles[3]);
+           //game.checkCirclePosition(circles[4]);
 
-            // TODO 9 : Iterate over the array
-            for (var i = 0; i < myArray.length; i++){
-                console.log(index[0]);
+            // TODO 9 : Iterate over the array)
+            for (var circ100 = 0; circ100 < circles.length; circ100+=1){
+                physikz.updatePosition(circles[circ100]);
+                game.checkCirclePosition(circles[circ100]);
             }
-           physikz.updatePosition(circles[i])
-           game.checkCirclePosition(circles[i])
+
             
         }
     
@@ -94,27 +95,22 @@ var init = function (window) {
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width + circle.radius ) {
-                circle.x = 0 - circle.radius
+
+   
+            // TODO 6 : YOUR CODE STARTS HERE ///////////////////////
+            if(circle.x > canvas.width){
+                circle.x = 0;
             }
-            
-            else if () {
-                
+            if(circle.x < 0){
+                circle.x = canvas.width
+
             }
-            // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            view.addChild(fps);
-            app.addUpdatable(fps);
-
-            runner.checkCircleBounds = function(circle){
-          
-
-            canvas.width
-            canvas.height
-
-             circle.x
-             circle.y
-             circle.radius
-        }
+            if(circle.y > canvas.height){
+                circle.y = 0;
+            }
+            if(circle.y < 0){
+                circle.y = canvas.height;
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
